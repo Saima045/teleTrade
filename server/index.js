1aac89d79dbfddbf86a5c3be0e157564ff49496a
Saima Nasir <122177642+Saima045@users.noreply.github.com>
2024-02-20 23:21:40 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const app=express()
 //app.use(cors())
 app.use(cors(
     {
-        origin:['https://tele-trade-frontend.vercel.app/'],
+        origin:['https://tele-trade-frontend.vercel.app'],
         methods:["POST","GET"],
         credentials:true
 
